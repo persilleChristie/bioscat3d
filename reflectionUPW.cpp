ee9644549850ca49f3ca7114ebe9a0141f563540
Tripleskull <tripleskull@bloodbowl.dk>
2025-03-19 18:49:18 +0100

    Update reflectionUPW.cpp

--- reflectionUPW.cpp
+++ reflectionUPW.cpp
@@ -3,10 +3,11 @@
 #include <complex>
 #include <array>
 #include <vector>
+#include <Eigen/Dense>
 
 using namespace std;
 
-const complex<double> j(0, 1);  // Imaginary unit
+const std::complex<double> j(0, 1);  // Imaginary unit
 
 // =========================================
 //  Constants Definition
@@ -50,9 +51,9 @@ yhat = np.array([0,1,0])
 zhat = np.array([0,0,1])
 */
 
-const array<double, 3> xhat = {1.0, 0.0, 0.0};
-const array<double, 3> yhat = {0.0, 1.0, 0.0};
-const array<double, 3> zhat = {0.0, 0.0, 1.0};
+const Eigen::Vector3d xhat = {1.0, 0.0, 0.0};
+const Eigen::Vector3d yhat = {0.0, 1.0, 0.0};
+const Eigen::Vector3d zhat = {0.0, 0.0, 1.0};
 
 // =========================================
 //  Azimuthal Angle Computation
@@ -63,12 +64,12 @@ def azimutal_angle(k_inc):
     sin_phi = np.sqrt(1 - cos_phi**2)
 */
 
-inline double cos_phi(const array<double, 3>& k_inc) {
+inline double cos_phi(const Eigen::Vector3d& k_inc) {
     double xy_norm = hypot(k_inc[0], k_inc[1]);
     return (xy_norm == 0) ? 1.0 : k_inc[0] / xy_norm;
 }
 
-inline double sin_phi(const array<double, 3>& k_inc) {
+inline double sin_phi(const Eigen::Vector3d& k_inc) {
     double xy_norm = hypot(k_inc[0], k_inc[1]);
     return (xy_norm == 0) ? 0.0 : k_inc[1] / xy_norm;
 }
@@ -83,8 +84,9 @@ def rotate(k_inc, E_inc, cos_phi, sin_phi):
                     [0, 0, 1]])
 */
 
-array<array<double, 3>, 3> rotation_matrix_z(double cos_phi, double sin_phi) {
-    return {{
+Eigen::Matrix3d rotation_matrix_z(double cos_phi, double sin_phi) {
+    return {
+        Eigen::Matrix3d{
         {cos_phi, -sin_phi, 0},
         {sin_phi, cos_phi,  0},
         {0,       0,        1}
@@ -100,11 +102,11 @@ def polar_angle(k_rot):
     sin_theta = np.sqrt(1 - cos_theta**2)
 */
 
-inline double cos_theta(const array<double, 3>& k_rot) {
+inline double cos_theta(const Eigen::Vector3d& k_rot) {
     return k_rot[2] / hypot(k_rot[0], k_rot[2]);
 }
 
-inline double sin_theta(const array<double, 3>& k_rot) {
+inline double sin_theta(const Eigen::Vector3d& k_rot) {
     double cosT = cos_theta(k_rot);
     return sqrt(1 - cosT * cosT);
 }
@@ -141,26 +143,20 @@ pair<double, double> fresnel_coeffs_TM(double cos_theta_inc, double sin_theta_in
 def reflected_field_TE(Gamma_r, cos_theta_inc, sin_theta_inc, E0, k1 = k_air):
 */
 
-array<double, 3> reflected_field_TE(double Gamma_r, double cos_theta_inc, double sin_theta_inc, double E0, double k1 = k_air) {
-    return {{
-        0.0,
-        Gamma_r * E0 * exp(-j * k1 * (sin_theta_inc - cos_theta_inc)),
-        0.0
-    }};
+Eigen::Vector3cd reflected_field_TE(double Gamma_r, double cos_theta_inc, double sin_theta_inc, double E0, double k1 = k_air) {
+    return Eigen::Vector3cd(0.0, Gamma_r * E0 * exp(-j * k1 * (sin_theta_inc - cos_theta_inc)), 0.0);
 }
+
 
 /*
 def transmitted_field_TE(Gamma_t, sin_theta_inc, E0, k1 = k_air, k2 = k_substrate):
 */
 
-array<double, 3> transmitted_field_TE(double Gamma_t, double sin_theta_inc, double E0, double k1 = k_air, double k2 = k_substrate) {
+Eigen::Vector3cd transmitted_field_TE(double Gamma_t, double sin_theta_inc, double E0, double k1 = k_air, double k2 = k_substrate) {
     double sin_theta_trans = k1 / k2 * sin_theta_inc;
     double cos_theta_trans = sqrt(1 - sin_theta_trans * sin_theta_trans);
-    return {{
-        0.0,
-        Gamma_t * E0 * exp(-j * k2 * (sin_theta_trans + cos_theta_trans)),
-        0.0
-    }};
+    return {Eigen::Vector3cd(0.0, Gamma_t * E0 * exp(-j * k2 * (sin_theta_trans + cos_theta_trans)), 0.0)
+    };
 }
 
 // =========================================
@@ -170,7 +166,7 @@ array<double, 3> transmitted_field_TE(double Gamma_t, double sin_theta_inc, doub
 def driver(k_inc, E_inc):
 */
 
-array<double, 3> driver(const array<double, 3>& k_inc, double E_inc) {
+Eigen::Vector3cd driver(const Eigen::Vector3d& k_inc, double E_inc) {
     double cosP = cos_phi(k_inc);
     double sinP = sin_phi(k_inc);
 
@@ -192,12 +188,12 @@ array<double, 3> driver(const array<double, 3>& k_inc, double E_inc) {
 //  Main Function
 // =========================================
 int main() {
-    array<double, 3> k_inc = {1.0, 0.0, 2.0};
+    Eigen::Vector3d k_inc(1.0, 0.0, 2.0);
     double E_inc = 2.0;
 
     auto E_tot = driver(k_inc, E_inc);
 
-    cout << "E_tot: (" << E_tot[0] << ", " << E_tot[1] << ", " << E_tot[2] << ")" << endl;
+    std::cout << "E_tot: (" << E_tot[0] << ", " << E_tot[1] << ", " << E_tot[2] << ")" << endl;
 
     return 0;
 }
