21c51f349fb33ae795b62576bbf3aa028d3bf29e
Tripleskull <tripleskull@bloodbowl.dk>
2025-03-19 14:07:39 +0100

    Eigen

--- moveDipole.cpp
+++ moveDipole.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <array>
 #include <vector>
+#include <algorithm>
+#include <Eigen>
 
 using namespace std;
 
@@ -17,16 +19,17 @@ def moveDipoleField(xPrime, rPrime , E, H ):
     sin_theta = np.sqrt(1 - cos_theta**2)
 */
 
-inline double norm(const array<double, 3>& x) {
+inline double norm(const Eigen::Vector3d& x) {
     return std::hypot(x[0], x[1], x[2]);
 }
 
-inline double cos_theta(const array<double, 3>& rPrime) {
+inline double cos_theta(const Eigen::Vector3d& rPrime) {
     return rPrime[2] / norm(rPrime);
 }
 
-inline double sin_theta(const array<double, 3>& rPrime) {
+inline double sin_theta(const Eigen::Vector3d& rPrime) {
     double cosT = cos_theta(rPrime);
+    cosT = std::clamp(cosT, -1.0, 1.0);
     return sqrt(1 - cosT * cosT);
 }
 
@@ -35,15 +38,16 @@ inline double sin_theta(const array<double, 3>& rPrime) {
     sin_phi = np.sqrt(1 - cos_phi**2)
 */
 
-inline double cos_phi(const array<double, 3>& rPrime) {
+inline double cos_phi(const Eigen::Vector3d& rPrime) {
     double xy_norm = std::hypot(rPrime[0], rPrime[1]);
     return (xy_norm == 0) ? 1.0 : rPrime[0] / xy_norm;
 }
 
-inline double sin_phi(const array<double, 3>& rPrime) {
+inline double sin_phi(const Eigen::Vector3d& rPrime) {
     double xy_norm = std::hypot(rPrime[0], rPrime[1]);
     return (xy_norm == 0) ? 0.0 : rPrime[1] / xy_norm;
 }
+
 
 // =========================================
 //  Compute Rotation Matrices
@@ -55,7 +59,7 @@ inline double sin_phi(const array<double, 3>& rPrime) {
                    [-sin_theta, 0, cos_theta]])
 */
 
-array<array<double, 3>, 3> rotation_matrix_y(const array<double, 3>& rPrime) {
+array<array<double, 3>, 3> rotation_matrix_y(const Eigen::Vector3d& rPrime) {
     double cosT = cos_theta(rPrime);
     double sinT = sin_theta(rPrime);
     return {{
@@ -72,7 +76,7 @@ array<array<double, 3>, 3> rotation_matrix_y(const array<double, 3>& rPrime) {
                    [0, 0, 1]])
 */
 
-array<array<double, 3>, 3> rotation_matrix_z(const array<double, 3>& rPrime) {
+Eigen::Matrix3d rotation_matrix_z(const Eigen::Vector3d& rPrime) {
     double cosP = cos_phi(rPrime);
     double sinP = sin_phi(rPrime);
     return {{
@@ -91,14 +95,6 @@ array<array<double, 3>, 3> rotation_matrix_z(const array<double, 3>& rPrime) {
     H_rot = lambda x: np.dot(Rz, np.dot(Ry, H(x)))
 */
 
-array<double, 3> rotate_vector(const array<double, 3>& v, const array<array<double, 3>, 3>& R) {
-    return {{
-        R[0][0] * v[0] + R[0][1] * v[1] + R[0][2] * v[2],
-        R[1][0] * v[0] + R[1][1] * v[1] + R[1][2] * v[2],
-        R[2][0] * v[0] + R[2][1] * v[1] + R[2][2] * v[2]
-    }};
-}
-
 // =========================================
 //  Compute Final Translated Fields
 // =========================================
@@ -107,12 +103,8 @@ array<double, 3> rotate_vector(const array<double, 3>& v, const array<array<doub
     H_prime = lambda x: H_rot(x) - xPrime
 */
 
-array<double, 3> translate_vector(const array<double, 3>& v, const array<double, 3>& xPrime) {
-    return {{
-        v[0] - xPrime[0],
-        v[1] - xPrime[1],
-        v[2] - xPrime[2]
-    }};
+Eigen::Vector3d translate_vector(const Eigen::Vector3d& v, const Eigen::Vector3d& xPrime) {
+    return v - xPrime;
 }
 
 // =========================================
@@ -122,26 +114,23 @@ array<double, 3> translate_vector(const array<double, 3>& v, const array<double,
     return E_prime, H_prime
 */
 
-pair<array<double, 3>, array<double, 3>> moveDipoleField(
-    const array<double, 3>& xPrime,
-    const array<double, 3>& rPrime,
-    const array<double, 3>& E,
-    const array<double, 3>& H
+pair<Eigen::Vector3d, Eigen::Vector3d> moveDipoleField(
+    const Eigen::Vector3d& xPrime,
+    const Eigen::Vector3d& rPrime,
+    const Eigen::Vector3d& E,
+    const Eigen::Vector3d& H
 ) {
     // Compute rotation matrices
     auto Ry = rotation_matrix_y(rPrime);
     auto Rz = rotation_matrix_z(rPrime);
 
-    // Rotate E and H fields
-    auto E_rot = rotate_vector(E, Ry);
-    E_rot = rotate_vector(E_rot, Rz);
+    // Rotate the field components
+    auto E_rot = Rz * Ry * E;
+    auto H_rot = Rz * Ry * H;
 
-    auto H_rot = rotate_vector(H, Ry);
-    H_rot = rotate_vector(H_rot, Rz);
-
-    // Translate the rotated fields
-    auto E_prime = translate_vector(E_rot, xPrime);
-    auto H_prime = translate_vector(H_rot, xPrime);
+    // Translate the fields
+    auto E_prime = E_rot - xPrime;
+    auto H_prime = H_rot - xPrime;
 
     return {E_prime, H_prime};
 }
@@ -151,10 +140,10 @@ pair<array<double, 3>, array<double, 3>> moveDipoleField(
 // =========================================
 int main() {
     // Define test inputs
-    array<double, 3> xPrime = {1.0, 1.0, 1.0};
-    array<double, 3> rPrime = {0.0, 1.0, 1.0};
-    array<double, 3> E = {2.0, -1.0, 3.0};
-    array<double, 3> H = {1.0, 2.0, -1.0};
+    Eigen::Vector3d xPrime(1.0, 1.0, 1.0);
+    Eigen::Vector3d rPrime(0.0, 1.0, 1.0);
+    Eigen::Vector3d E(2.0, -1.0, 3.0);
+    Eigen::Vector3d H(1.0, 2.0, -1.0);
 
     // Compute moved dipole fields
     auto [E_new, H_new] = moveDipoleField(xPrime, rPrime, E, H);
