05b72a1737453b3d882d21139cc9d6c47b195623
persilleChristie <pc@pchristie.dk>
2025-03-25 16:06:36 +0100

    rettet

--- matrixToCSVfile.h
+++ matrixToCSVfile.h
@@ -1,3 +1,6 @@
+#ifndef _MATRIX_TO_CSV_H
+#define _MATRIX_TO_CSV_H
+
 #include <Eigen/Dense>
 #include <iostream>
 #include <fstream>
@@ -7,14 +10,7 @@ using namespace std;
 
 const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
 
-// writing functions taking Eigen types as parameters, 
-// see https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
 template <typename Derived>
+void matrixToCSVfile(string name, const Eigen::MatrixBase<Derived>& matrix); // std::list<string> column_names,
 
-void matrixToCSVfile(string name, std::list<string> column_names, const Eigen::MatrixBase<Derived>& matrix)
-{
-    ofstream file(name.c_str());
-    file << matrix.format(CSVFormat);
-    // file.close() is not necessary, 
-    // desctructur closes file, see https://en.cppreference.com/w/cpp/io/basic_ofstream
-}
+#endif
