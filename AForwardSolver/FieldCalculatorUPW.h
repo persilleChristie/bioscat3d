5ffb945b58562582ebc530402c27d8dabd1eca3c
Tripleskull <tripleskull@bloodbowl.dk>
2025-03-27 11:39:15 +0100

    -

---
+++ AForwardSolver/FieldCalculatorUPW.h
@@ -0,0 +1,23 @@
+#ifndef UPWFIELDCALCULATOR_H
+#define UPWFIELDCALCULATOR_H
+
+#include "FieldCalculator.h"
+#include <Eigen/Dense>
+
+class UPWFieldCalculator : public FieldCalculator {
+public:
+    UPWFieldCalculator(const Eigen::Vector3d& waveVector,
+                       const Eigen::Vector3d& polarization);
+
+    void computeFields(
+        Eigen::MatrixXd& outE,
+        Eigen::MatrixXd& outH,
+        const Eigen::MatrixXd& evalPoints
+    ) const override;
+
+private:
+    Eigen::Vector3d k;   // wave vector
+    Eigen::Vector3d E0;  // polarization vector
+};
+
+#endif // UPWFIELDCALCULATOR_H
