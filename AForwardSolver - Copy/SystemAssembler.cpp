2707672fa60577bfbfd2f66d0b80fd67de1c2344
persilleChristie <pc@pchristie.dk>
2025-04-01 15:48:26 +0200

    update systemsolver

--- AForwardSolver - Copy/SystemAssembler.cpp
+++ AForwardSolver - Copy/SystemAssembler.cpp
@@ -12,17 +12,23 @@ void SystemAssembler::assembleSystem(
     Eigen::MatrixX3cd& A,
     Eigen::VectorXcd& b,
     const Surface& surface,
-    const std::vector<std::shared_ptr<FieldCalculator>>& sources,
-    const std::shared_ptr<FieldCalculator>& incident
+    const std::vector<std::shared_ptr<FieldCalculator>>& sources_int,
+    const std::vector<std::shared_ptr<FieldCalculator>>& sources_mirr,
+    const std::vector<std::shared_ptr<FieldCalculator>>& sources_ext,
+    const std::shared_ptr<FieldCalculator>& incident,
+    const std::complex<double> Gamma_r
 ) {
     const auto& points = surface.getPoints();
     const auto& tau1 = surface.getTau1();
     const auto& tau2 = surface.getTau2();
 
     int M = points.rows();
-    int N = sources.size();
-    
-    A.resize(4 * M, N);
+    int Nprime_double = sources_int.size();
+    int N2prime_double = sources_ext.size();
+    int Nprime = Nprime_double/2;
+    int N2prime = N2prime_double/2;
+
+    A.resize(4 * M, Nprime_double + N2prime_double);
     b.resize(4 * M);
 
     MatrixX3cd E_inc_new(M,3);
@@ -39,22 +45,98 @@ void SystemAssembler::assembleSystem(
         b(mu + M)   = -E_inc_new.row(mu).dot(t2);
         b(mu + 2*M) = -H_inc_new.row(mu).dot(t1);
         b(mu + 3*M) = -H_inc_new.row(mu).dot(t2);
+        
 
-        for (int nu = 0; nu < N; ++nu) {
+        for (int nu = 0; nu < Nprime; ++nu) {
             MatrixX3cd E_HD(1,3);
             MatrixX3cd H_HD(1,3);
 
-            sources[nu]->computeFields(E_HD, H_HD, x_mu);
+            sources_int[nu]->computeFields(E_HD, H_HD, x_mu);
 
-            Vector3cd E = E_HD.row(0);
-            Vector3cd H = H_HD.row(0);
-            // Vector3cd E = sources[nu]->getEField(x_mu);
-            // Vector3cd H = sources[nu]->getHField(x_mu);
+            Vector3cd E_int1 = E_HD.row(0);
+            Vector3cd H_int1 = H_HD.row(0);
 
-            A(mu, nu)       = E.dot(t1);
-            A(mu + M, nu)   = E.dot(t2);
-            A(mu + 2*M, nu) = H.dot(t1);
-            A(mu + 3*M, nu) = H.dot(t2);
+            sources_int[nu + Nprime]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E_int2 = E_HD.row(0);
+            Vector3cd H_int2 = H_HD.row(0);
+            
+            sources_mirr[nu]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E_mirr1 = E_HD.row(0);
+            Vector3cd H_mirr1 = H_HD.row(0);
+
+            sources_mirr[nu + Nprime]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E_mirr2 = E_HD.row(0);
+            Vector3cd H_mirr2 = H_HD.row(0);
+
+            // Electric fields
+            // A(1,1) 
+            A(mu, nu) = E_int1.dot(tau1) + Gamma_r * E_mirr1.dot(tau1);
+            
+            // A(1,2)
+            A(mu, nu + Nprime) = E_int2.dot(tau1) + Gamma_r * E_mirr2.dot(tau1);
+
+            // A(2,1)
+            A(mu + M, nu) = E_int1.dot(tau2) + Gamma_r * E_mirr1.dot(tau2);
+            
+            // A(2,2)
+            A(mu + M, nu + Nprime) = E_int2.dot(tau2) + Gamma_r * E_mirr2.dot(tau2);
+
+            // Magnetic fields
+            // A(3,1)
+            A(mu + 2*M, nu) = H_int1.dot(tau1) + Gamma_r * H_mirr1.dot(tau1);
+            
+            // A(3,2)
+            A(mu + 2*M, nu + Nprime) = H_int2.dot(tau1) + Gamma_r * H_mirr2.dot(tau1);
+
+            // A(4,1)
+            A(mu + 3*M, nu) = H_int1.dot(tau2) + Gamma_r * H_mirr1.dot(tau2);
+            
+            // A(4,2)
+            A(mu + 3*M, nu + Nprime) = H_int2.dot(tau2) + Gamma_r * H_mirr2.dot(tau2);
+        }
+
+        for (int nu = 0; nu < N2prime; ++nu) {
+            MatrixX3cd E_HD(1,3);
+            MatrixX3cd H_HD(1,3);
+
+            sources_ext[nu]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E_ext1 = E_HD.row(0);
+            Vector3cd H_ext1 = H_HD.row(0);
+
+            sources_ext[nu + N2prime]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E_ext2 = E_HD.row(0);
+            Vector3cd H_ext2 = H_HD.row(0);
+
+            // Electric fields
+            // A(1,3) 
+            A(mu, nu + 2*Nprime) = E_ext1.dot(tau1);
+            
+            // A(1,4)
+            A(mu, nu + 2*Nprime + N2prime) = E_ext2.dot(tau1);
+
+            // A(2,3)
+            A(mu + M, nu + 2*Nprime) = E_ext1.dot(tau2);
+            
+            // A(2,4)
+            A(mu + M, nu + 2*Nprime + N2prime)  = E_ext2.dot(tau2);
+
+            // Magnetic fields
+            // A(3,3)
+            A(mu + 2*M, nu + 2*Nprime) = H_ext1.dot(tau1);
+            
+            // A(3,4)
+            A(mu + 2*M, nu + 2*Nprime + N2prime) = H_ext2.dot(tau1);
+
+            // A(4,3)
+            A(mu + 3*M, nu + 2*Nprime) = H_ext1.dot(tau2);
+            
+            // A(4,4)
+            A(mu + 3*M, nu + 2*Nprime + N2prime) = H_ext2.dot(tau2);
         }
     }
 }
--- AForwardSolver - Copy/SystemAssembler.h
+++ AForwardSolver - Copy/SystemAssembler.h
@@ -13,8 +13,11 @@ public:
         Eigen::MatrixX3cd& A,
         Eigen::VectorXcd& b,
         const Surface& surface_mu,
-        const std::vector<std::shared_ptr<FieldCalculator>>& sources,
-        const std::shared_ptr<FieldCalculator>& incident
+        const std::vector<std::shared_ptr<FieldCalculator>>& sources_int,
+        const std::vector<std::shared_ptr<FieldCalculator>>& sources_mirr,
+        const std::vector<std::shared_ptr<FieldCalculator>>& sources_ext,
+        const std::shared_ptr<FieldCalculator>& incident,
+        const std::complex<double> Gamma_r
     );
 };
 
