b983b4cc5103a639240e4e69e8bd1495901f04ab
persilleChristie <pc@pchristie.dk>
2025-04-01 15:12:54 +0200

    fejl i systemassembler

--- AForwardSolver - Copy/SystemAssembler.cpp
+++ AForwardSolver - Copy/SystemAssembler.cpp
@@ -3,6 +3,7 @@
 #include "FieldCalculator.h"
 #include "FieldCalculatorDipole.h"
 #include "FieldCalculatorUPW.h"
+#include "Constants.h"
 #include <iostream>
 
 using namespace Eigen;
@@ -20,25 +21,35 @@ void SystemAssembler::assembleSystem(
 
     int M = points.rows();
     int N = sources.size();
+    
     A.resize(4 * M, N);
     b.resize(4 * M);
 
+    MatrixX3cd E_inc_new(M,3);
+    MatrixX3cd H_inc_new(M,3);
+
+    incident->computeFields(E_inc_new, H_inc_new, points);
+
     for (int mu = 0; mu < M; ++mu) {
-        Vector3d x_mu = points.row(mu);
+        MatrixX3d x_mu = points.row(mu);
         Vector3d t1 = tau1.row(mu);
         Vector3d t2 = tau2.row(mu);
 
-        Vector3cd E_inc = incident->getEField(x_mu);
-        Vector3cd H_inc = incident->getHField(x_mu);
-
-        b(mu)       = -E_inc.dot(t1);
-        b(mu + M)   = -E_inc.dot(t2);
-        b(mu + 2*M) = -H_inc.dot(t1);
-        b(mu + 3*M) = -H_inc.dot(t2);
+        b(mu)       = -E_inc_new.row(mu).dot(t1);
+        b(mu + M)   = -E_inc_new.row(mu).dot(t2);
+        b(mu + 2*M) = -H_inc_new.row(mu).dot(t1);
+        b(mu + 3*M) = -H_inc_new.row(mu).dot(t2);
 
         for (int nu = 0; nu < N; ++nu) {
-            Vector3cd E = sources[nu]->getEField(x_mu);
-            Vector3cd H = sources[nu]->getHField(x_mu);
+            MatrixX3cd E_HD(1,3);
+            MatrixX3cd H_HD(1,3);
+
+            sources[nu]->computeFields(E_HD, H_HD, x_mu);
+
+            Vector3cd E = E_HD.row(0);
+            Vector3cd H = H_HD.row(0);
+            // Vector3cd E = sources[nu]->getEField(x_mu);
+            // Vector3cd H = sources[nu]->getHField(x_mu);
 
             A(mu, nu)       = E.dot(t1);
             A(mu + M, nu)   = E.dot(t2);
--- AForwardSolver - Copy/main.cpp
+++ AForwardSolver - Copy/main.cpp
@@ -14,14 +14,13 @@ int main() {
     using namespace Eigen;
 
     // Define parameters
-    int num_points = 10;
     double radius = 1.0;
     Vector3d center(0.0, 0.0, 1.0);
-    double k0 = 2 * constants.pi;
-    double Gamma_r = 1.0;
     Vector3d k_inc(0.0, 0.0, -1.0);
     int resolution = 5;
     double polarization = 0.2;
+    double E0 = 1.0;
+    Constants constants;
     
     
     // create spheres
@@ -34,11 +33,13 @@ int main() {
 
 
     // call System Assembler
+    int M = sphere_mu.getPoints().rows();
+    int N = sphere_nu_prime.getPoints().rows() + sphere_nu_2prime.getPoints().rows();
     
-    Eigen::MatrixX3cd A;
-    Eigen::VectorXcd b;
+    Eigen::MatrixX3cd A(4 * M, N);
+    Eigen::VectorXcd b(4 *M);
 
-    std::shared_ptr<FieldCalculatorUPW> incident = std::make_shared<FieldCalculatorUPW>(k_inc, polarization);
+    std::shared_ptr<FieldCalculatorUPW> incident = std::make_shared<FieldCalculatorUPW>(k_inc, E0, polarization, constants);
 
     std::vector<std::shared_ptr<FieldCalculator>> sources;
 
@@ -46,9 +47,10 @@ int main() {
         const MatrixXd& pts = surface.getPoints();
         const MatrixXd& t1 = surface.getTau1();
         const MatrixXd& t2 = surface.getTau2();
+
         for (int i = 0; i < pts.rows(); ++i) {
-            sources.emplace_back(std::make_shared<FieldCalculatorDipole>(Dipole(pts.row(i), t1.row(i))));
-            sources.emplace_back(std::make_shared<FieldCalculatorDipole>(Dipole(pts.row(i), t2.row(i))));
+            sources.emplace_back(std::make_shared<FieldCalculatorDipole>(Dipole(pts.row(i), t1.row(i)), constants));
+            sources.emplace_back(std::make_shared<FieldCalculatorDipole>(Dipole(pts.row(i), t2.row(i)), constants));
         }
     };
 
