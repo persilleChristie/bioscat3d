67a6e40573fad03b33870684c2c9b23f612e9d12
Tripleskull <--global>
2025-05-22 07:41:43 +0200

    -

--- Python/src/Spline.py
+++ Python/src/Spline.py
@@ -30,21 +30,11 @@ class Spline:
 
         return np.max(abs(H))
     
-    def calculate_points(self, resolution):
-        x = np.linspace(self.Xfine.min(), self.Xfine.max(), resolution)
-        y = np.linspace(self.Yfine.min(), self.Yfine.max(), resolution)
-
-        x_control = np.linspace(self.Xfine.min() + (x[1]-x[0])/2, self.Xfine.max()- (x[1]-x[0])/2, resolution - 1)
-        y_control = np.linspace(self.Yfine.min() + (y[1]-y[0])/2, self.Yfine.max()- (y[1]-y[0])/2, resolution - 1)
-
+    def __calculate_normals_tangents__(self, x, y):
         X, Y = np.meshgrid(x, y)
         Z = self.__evaluate_at_points__(x, y)
 
-        X_control, Y_control = np.meshgrid(x_control, y_control)
-        Z_control  = self.__evaluate_at_points__(x_control, y_control)
-
-        test_points = np.column_stack((X.ravel(), Y.ravel(), Z.ravel()))
-        control_points = np.column_stack((X_control.ravel(), Y_control.ravel(), Z_control.ravel()))
+        points = np.column_stack((X.ravel(), Y.ravel(), Z.ravel()))
 
         # Compute normals
         fx = self.__evaluate_at_points__(x, y, dx = 1).ravel()
@@ -65,7 +55,30 @@ class Spline:
         # Compute second tangent as cross product
         tangent2 = np.cross(normals, tangent1)
 
-        return test_points, normals, tangent1, tangent2, control_points
+        return normals, tangent1, tangent2
+    
+    def calculate_points(self, resolution, control_points_flag = False):
+        x = np.linspace(self.Xfine.min(), self.Xfine.max(), resolution)
+        y = np.linspace(self.Yfine.min(), self.Yfine.max(), resolution)
+
+        X, Y = np.meshgrid(x, y)
+        Z = self.__evaluate_at_points__(x, y)
+        test_points = np.column_stack((X.ravel(), Y.ravel(), Z.ravel()))
+
+
+        normals, tangent1, tangent2 = self.__calculate_normals_tangents__(x, y)
+        if control_points_flag:
+            control_x = np.linspace(self.Xfine.min() + (x[1]-x[0])/2, self.Xfine.max()- (x[1]-x[0])/2, resolution - 1)
+            control_y = np.linspace(self.Yfine.min() + (y[1]-y[0])/2, self.Yfine.max()- (y[1]-y[0])/2, resolution - 1)
+            
+            X_control, Y_control = np.meshgrid(control_y, control_y)
+            Z_control  = self.__evaluate_at_points__(control_x, control_y)
+            control_points = np.column_stack((X_control.ravel(), Y_control.ravel(), Z_control.ravel()))
+
+            _, control_tangent1, control_tangent2 = self.__calculate_normals_tangents__(control_x, control_y)
+            return test_points, normals, tangent1, tangent2, control_points, control_tangent1, control_tangent2
+
+        return test_points, normals, tangent1, tangent2
 
 
 
