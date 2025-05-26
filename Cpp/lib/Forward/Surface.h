1ee76b5065ce46a1d483d58f2129599739e8549d
PernilleChristie <pc@pchristie.dk>
2025-05-26 07:59:29 +0200

    fjernet mirrored i surface

--- Cpp/lib/Forward/Surface.h
+++ Cpp/lib/Forward/Surface.h
@@ -12,8 +12,6 @@ public:
     virtual const Eigen::MatrixXd& getNormals() const = 0;
     virtual const Eigen::MatrixXd& getTau1() const = 0;
     virtual const Eigen::MatrixXd& getTau2() const = 0;
-
-    virtual std::unique_ptr<Surface> mirrored(const Eigen::Vector3d& normal) const = 0;
 };
 
 #endif // SURFACE_H
